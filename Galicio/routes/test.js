e001e9833cb5d1c26e3759cb35f3d19429801f32
AleCaeiro <acaeiro@itba.edu.ar>
2023-06-29 15:54:16 -0300

    Implmenting model galicio

--- Galicio/routes/test.js
+++ Galicio/routes/test.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const Account = require('../models/Account');
+//testing
 
 router.get('/', (req, res) => {
     res.send({welcome: "Buenos Días :)"});
